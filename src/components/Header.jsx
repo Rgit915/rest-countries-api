7d9c1b0e5e419eeb60671e9942c56d9437c80206
Rgit915 <rora.goitom@gmail.com>
2025-05-22 15:48:07 -0400

    Move Header to a Reusable Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Header = ({darkMode, setDarkMode}) => {
+  return (
+    <header className="flex justify-between py-8 px-6 lg:px-12">
+        <h1 className="font-extrabold leading-5 text-black-800  dark:text-white">
+          Where in the world?
+        </h1>
+
+        <button
+          className="flex items-center gap-3 cursor-pointer"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+          {darkMode ? (
+            <>
+              <i className="fa-solid fa-moon"></i>
+              <p className="font-semibold text-[12px] text-black-800 dark:text-white">
+                Dark Mode
+              </p>
+            </>
+          ) : (
+            <>
+              <i className="fa-regular fa-moon"></i>
+              <p className="font-semibold text-[12px] text-black-800 dark:text-white">
+                Light Mode
+              </p>
+            </>
+          )}
+        </button>
+      </header>
+  )
+}
+
+export default Header
